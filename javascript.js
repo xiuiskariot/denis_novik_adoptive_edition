d51fb8a4ba15246267b82a6f2b74788d38ca06c1
Ksusha <xiuschv@yandex.ru>
2023-03-08 21:52:01 +0300

    add not-adoptive burger menu

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu");
+
+menuBtn.onclick = function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+};
