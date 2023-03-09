7bf1e6c2df2c69c2a3fe3fd77eebc5b1485d874c
Ksusha <xiuschv@yandex.ru>
2023-03-09 13:00:51 +0300

    add burder menu

--- javascript.js
+++ javascript.js
@@ -1,7 +1,33 @@
-let menuBtn = document.querySelector(".menu-btn");
-let menu = document.querySelector(".menu");
+const hamb = document.querySelector(".hamb__field");
+const popup = document.querySelector(".popup");
 
-menuBtn.onclick = function () {
-  menuBtn.classList.toggle("active");
-  menu.classList.toggle("active");
-};
+const menu = document.querySelector(".menu").cloneNode(1);
+
+
+hamb.addEventListener('click', hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  hamb.classList.toggle("active");
+  popup.classList.toggle("open");
+  renderPopup();
+  body.classList.toggle("noscroll");
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
+
--- кнопка/javascript.js
+++
@@ -1,7 +0,0 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
-
-menuBtn.onclick = function () {
-  menuBtn.classList.toggle("active");
-  menu.classList.toggle("active");
-}
